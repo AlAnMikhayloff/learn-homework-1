8d522f5e9d3ea51da9db9472738bce368234363f
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-09-08 22:36:23 +0300

    Update 1_if1.py

--- 1_if1.py
+++ 1_if1.py
@@ -19,7 +19,17 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    age_user = input('What is your age? ')
+    age_user = int(age_user)
+    if 0 < age_user <= 6:
+        activity_user = "Do you really go to kindergarten?"
+    if 7 < age_user <= 18:
+        activity_user = "Wow! You're going to school!"
+    if 18 < age_user <= 24:
+        activity_user = "Wow! You're going to college!"
+    else:
+        activity_user = "I hope you're not unemployed?"
+    print(activity_user)
 
 if __name__ == "__main__":
     main()
