34dc341a245e19fabea13894cb83228f7e9b224e
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-09-11 00:28:49 +0300

    Update 7_exception2.py

--- 7_exception2.py
+++ 7_exception2.py
@@ -17,8 +17,21 @@ def discounted(price, discount, max_discount=20):
     """
     Замените pass на ваш код
     """
-    pass
-    
+
+    try:
+        price = float(price)
+        discount = float(discount)
+        max_discount = int(max_discount)
+    except (ValueError, TypeError) as e:
+        print('Ошибка данных!')
+    if max_discount >= 100:
+        raise ValueError("Максимальная скидка не должна быть больше 100")
+    if discount >= max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price - (price * discount / 100)
+    return price_with_discount
+
 if __name__ == "__main__":
     print(discounted(100, 2))
     print(discounted(100, "3"))
