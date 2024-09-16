35fed0a10d2cf7ef6590a0cbd1cf73df70921bcd
AlAnMikhayloff <alanmikhayloff@gmail.com>
2024-09-16 13:29:40 +0300

    Update 8_ephem_bot.py

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -15,6 +15,7 @@
 import logging
 import ephem
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from datetime import datetime
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -39,16 +40,48 @@ def greet_user(update, context):
 def talk_to_me(update, context):
     user_text = update.message.text
     print(user_text)
-    update.message.reply_text(text)
+    update.message.reply_text(user_text)
 
+def talk_about_planet(update, context):
+    name_planet = update.message.text.split('/planet ')[1]
+    update.message.reply_text(name_planet)
+    
+
+    if name_planet.lower() == 'mars':
+        planet = ephem.Mars()
+    elif name_planet.lower() == 'venus':
+        planet = ephem.Venus()
+    elif name_planet.lower() == 'mercury':
+        planet = ephem.Mercury()
+    elif name_planet.lower() == 'saturn':
+        planet = ephem.Saturn()
+    elif name_planet.lower() == 'jupiter':
+        planet = ephem.Jupiter()
+    elif name_planet.lower() == 'uranus':
+        planet = ephem.Uranus()
+    elif name_planet.lower() == 'moon':
+        planet = ephem.Moon()
+    elif name_planet.lower() == 'neptune':
+        planet = ephem.Neptune()
+    elif name_planet.lower() == 'pluto':
+        planet = ephem.Pluto()   
+    else:
+        update.message.reply_text('Неизвестная планета.')
+    print(datetime.today())
+    planet.compute(datetime.today())
+    update.message.reply_text(ephem.constellation(planet))
+    print(planet, ephem.constellation(planet))
 
 def main():
     mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", talk_about_planet))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(MessageHandler(Filters.text, talk_about_planet))
 
+    logging.info('Бот стартовал.')
     mybot.start_polling()
     mybot.idle()
 
